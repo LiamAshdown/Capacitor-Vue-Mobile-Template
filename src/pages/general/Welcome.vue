691795131415f2eb1052a8d143b17efd31772fd9
Liam Ashdown <liam-ashdown@outlook.com>
2022-02-06 15:44:05 +0000

    Update template

--- src/pages/general/Welcome.vue
+++ src/pages/general/Welcome.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="welcome">
-    Welcome
+    <div class="content">
+      <h1>Vue Mobile Template</h1>
+      <p>Capacitor is installed, but you need to run:</p>
+      <pre><code class="language-markup">npm start</code></pre>
+
+      <p>You are also welcome to delete this page, and the javascript (prism) in index.html</p>s
+    </div>
   </div>
 </template>
 
@@ -10,9 +16,38 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+html {
+  background-color: #1f2933;
+  color: white;
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: content-box;
+    font-family: "Open Sans", sans-serif;
+  }
+}
+
 .welcome {
-  font-size: 50px;
   text-align: center;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -120px;
+  padding: 20px;
+
+  .content {
+    margin: 20px auto;
+    padding: 50px;
+    border-radius: 25px;
+    box-shadow: 5px 5px 20px #191f26;
+    background-color: #323f4b;
+
+    p {
+      margin: 20px 0;
+    }
+  }
 }
 </style>
